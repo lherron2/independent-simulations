209c1f091225d8d64d1693444c93e81ca2fa36eb
lherron2 <lherron@umd.edu>
2023-03-10 15:59:57 -0500

    added scripts to fix rosetta+rcsb pdb files

---
+++ openmm/fix_pdb.py
@@ -0,0 +1,19 @@
+from pdbfixer import PDBFixer
+from openmm.app import PDBFile
+
+def repair_pdb(pdb_in, pdb_out):
+    fixer = PDBFixer(filename=pdb_in)
+    fixer.findMissingResidues()
+    fixer.findNonstandardResidues()
+    fixer.replaceNonstandardResidues()
+    fixer.removeHeterogens(True)
+    fixer.findMissingAtoms()
+    fixer.addMissingAtoms()
+    fixer.addMissingHydrogens(7)
+    PDBFile.writeFile(fixer.topology, fixer.positions, open(pdb_out, 'w'))
+    
+pdbid = "1anr"
+# paths to pdb files -- change as needed
+pdb_in = f'/Users/lukasherron/research/RNAfold/{pdbid}/rosetta/{pdbid}_struct{i}.pdb'
+pdb_out = f'/Users/lukasherron/research/RNAfold/{pdbid}/rosetta/fixed/{pdbid}_struct{i}.pdb'
+repair_pdb(pdb_in, pdb_out)
---
+++ openmm/parameterize_system.py
@@ -0,0 +1,14 @@
+from openmm.app import *
+from openmm import *
+from openmm.unit import *
+
+pdb_in = "/PATH/TO/INPUT/PDB"
+pdb_out = "/PATH/TO/OUTPUT/PDB"
+
+pdb = PDBFile(pdb_in)
+forcefield = ForceField('amber14/RNA.Shaw_charmm22-ions.xml', 'amber14/tip4pd_desres.xml')
+modeller = Modeller(pdb.topology, pdb.positions)
+modeller.addExtraParticles(forcefield)
+
+PDBFile.writeFile(modeller.topology, modeller.positions, 
+                  open(pdb_out, 'w'))
