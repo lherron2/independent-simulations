01d9b27341fba7a87a895ff376c67145bde02d27
lherron2 <lherron@umd.edu>
2023-04-07 15:40:27 -0400

    made file formatting consistent

--- openmm/scripts/submit_remove_solvent.sh
+++ openmm/scripts/submit_remove_solvent.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -t 00:10:00
+#SBATCH -t 00:30:00
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=fold
 #SBATCH --mail-type=NONE    # Send email at begin and end of job
@@ -11,4 +11,8 @@ conda activate openmm
 pdb=$1
 structid=$2
 
-python -u remove_solvent.py --pdbid $pdb --structid $structid
+PREFIX="../example" 
+master_yaml="${PREFIX}/yaml/master_prod.yaml"
+sim_yaml="${PREFIX}/struct${structid}/sim_prod.yaml"
+
+python -u ../src/remove_solvent.py --pdbid $pdb --structid $structid
--- openmm/src/remove_solvent.py
+++ openmm/src/remove_solvent.py
@@ -5,22 +5,22 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--pdbid', required=True, 
+parser.add_argument('--pdbid', required=True,
                     type=str, help="master yaml file is required")
-parser.add_argument('--structid', required=True, 
+parser.add_argument('--structid', required=True,
                     type=str, help="simulation specific yaml file is required")
 args = parser.parse_args()
-traj_filename = f'structure{args.structid}/{args.pdbid}_struct{args.structid}.xtc'
-top_filename = f'structure{args.structid}/{args.pdbid}_struct{args.structid}_sol.pdb'
+traj_filename = f'struct{args.structid}/{args.pdbid}_struct{args.structid}.xtc'
+top_filename = f'struct{args.structid}/{args.pdbid}_struct{args.structid}_sol.pdb'
 
 try:
     for i, chunk in enumerate(md.iterload(traj_filename, top=top_filename, chunk=1000)):
         if i == 0:
-            no_sol = chunk.remove_solvent() 
+            no_sol = chunk.remove_solvent()
         else:
             no_sol = no_sol.join(chunk.remove_solvent())
 except: None
-no_sol = no_sol.center_coordinates()    
+no_sol = no_sol.center_coordinates()
 no_sol = no_sol.superpose(no_sol).smooth(3)
-no_sol.save_xtc(f'structure{args.structid}/{args.pdbid}_struct{args.structid}_nosol.xtc')
+no_sol.save_xtc(f'struct{args.structid}/{args.pdbid}_struct{args.structid}_nosol.xtc')
 print("saved")
