932ca7edee05523aa90c13854a22ac8cf2349e62
lherron2 <lherron@umd.edu>
2023-05-22 15:18:55 -0400

    bug fix

--- openmm/scripts/postprocess_simulations.sh
+++ openmm/scripts/postprocess_simulations.sh
@@ -47,17 +47,16 @@ source $HOME/.bashrc
 source ../sourceme.sh
 conda activate analysis
 
+echo $pdb
+
 root_path="${PROJECT_PATH}/${pdb}/${pdb}_iter${iter}"
 topology="${root_path}/${pdb}_nosol.pdb"
 master_config="${root_path}/master_prod.yaml"
 data_path="${PROJECT_PATH}/${pdb}/data/"
 
-# change according to your file system
-src_path="/home/lherron/scratch/repos/independent-simulations/openmm/src"
-
 cp "${root_path}/struct0/${pdb}_struct0_nosol.pdb" "${root_path}/${pdb}_nosol.pdb"
 
-python -u ${src_path}/postprocess_simulations.py --root_path $root_path \
+python -u ${SRCPATH}/postprocess_simulations.py --root_path $root_path \
                                                  --data_path $data_path \
                                                  --topology $topology \
                                                  --master_config $master_config \
