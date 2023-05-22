da5e2533238c0120062df26709cb2fbac53be6cb
lherron2 <lherron@umd.edu>
2023-05-22 14:39:25 -0400

    updated documentation

--- openmm/scripts/combine_energies.sh
+++ openmm/scripts/combine_energies.sh
@@ -5,25 +5,67 @@
 #SBATCH --mail-type=NONE    # Send email at begin and end of job
 #SBATCH --output=combine.out
 
+DOCSTRING=$"""
+Combines energy files across simulation indices into a single file.\n
+\n
+Args:\n
+--pdb: The pdb ID of the structure (or some other identifier).\n
+--iter: The 'round' of simulations being performed.\n
+--rad: Cutoff radius of the solvation shell.\n
+"""
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -h|--help)
+      echo -e $DOCSTRING
+      exit 1
+      ;;
+    --pdb)
+      pdb="$2"
+      shift
+      shift
+      ;;
+    --iter)
+      pdb="$2"
+      shift
+      shift
+      ;;
+    --rad)
+      pdb="$2"
+      shift
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
 source $HOME/.bashrc
 source ../sourceme.sh
-# sourceme contains $CONDA and $PROJECT_PATH
-#. "${CONDA_PREFIX_1}/etc/profile.d/conda.sh"
 conda activate analysis
 
 # INPUTS
-pdb=$1
 parameter="temperature"
-replica_path="${PROJECT_PATH}/${pdb}/${pdb}_iter0/"
+replica_path="${PROJECT_PATH}/${pdb}/${pdb}_iter${iter}/"
 out_path="${PROJECT_PATH}/${pdb}/data/"
 master_config="${replica_path}/master_prod.yaml"
-file_identifier="r=$2_energies"
+file_identifier="r=${rad}_energies"
 outfile="${pdb}_${file_identifier}"
 
-python -u ../src/combine_files.py --pdbid $pdb \
-                                --root_path $replica_path \
-                                --master_config $master_config \
-                                --file_identifier $file_identifier \
-                                --out_path $out_path \
-                                --outfile $outfile \
+python -u "${SRCPATH}/combine_files.py" --pdbid $pdb \
+                                        --root_path $replica_path \
+                                        --master_config $master_config \
+                                        --file_identifier $file_identifier \
+                                        --out_path $out_path \
+                                        --outfile $outfile \
 
