52e0931cb6ecbd0b59f77009be51341ded4874b1
lherron2 <lherron@umd.edu>
2023-05-22 15:11:11 -0400

    added option to specify project path in configure.sh

--- openmm/configure.sh
+++ openmm/configure.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
+PROJECTPATH="$PWD/experiments"
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --project-path)
+      PROJECTPATH="$2"
+      shift
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+
+
 sed "s+REPOPATH+$PWD+g" templates/sourceme_template.sh > sourceme.sh
+sed "s+PROJECTPATH+$PROJECTPATH+g" templates/sourceme_template.sh > sourceme.sh
