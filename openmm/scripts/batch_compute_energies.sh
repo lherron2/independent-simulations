99aa344ac6775f599fff54df68ebaf17b3313ce9
lherron2 <lherron@umd.edu>
2023-05-22 14:49:01 -0400

    update

--- openmm/scripts/batch_compute_energies.sh
+++ openmm/scripts/batch_compute_energies.sh
@@ -16,7 +16,7 @@ for pdb in "${pdbids[@]}"; do
     for rad in "${rads[@]}"; do
         for i in {0..49}; do 
             echo $pdb $rad $i
-            sbatch compute_solvated_energy.sh $pdb $i $rad;
+            sbatch compute_solvated_energy.sh --pdb $pdb --structid $i --rad $rad;
             sleep 0.1
         done
     done
